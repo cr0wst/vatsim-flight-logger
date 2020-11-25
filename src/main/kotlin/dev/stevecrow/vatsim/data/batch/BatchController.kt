1ec0411e534021bd5b8fc2a1925cd571469660a6
Steve Crow <steve@smcrow.net>
2020-11-24 20:27:48 -0500

    Enable CORS

--- src/main/kotlin/dev/stevecrow/vatsim/data/batch/BatchController.kt
+++ src/main/kotlin/dev/stevecrow/vatsim/data/batch/BatchController.kt
@@ -1,9 +1,11 @@
 package dev.stevecrow.vatsim.data.batch
 
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin
 class BatchController(
     val batchRepository: BatchRepository
 ) {
--- src/main/kotlin/dev/stevecrow/vatsim/data/flight/FlightController.kt
+++ src/main/kotlin/dev/stevecrow/vatsim/data/flight/FlightController.kt
@@ -1,9 +1,11 @@
 package dev.stevecrow.vatsim.data.flight
 
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin
 @RestController
 class FlightController(
     val flightRepository: FlightRepository
