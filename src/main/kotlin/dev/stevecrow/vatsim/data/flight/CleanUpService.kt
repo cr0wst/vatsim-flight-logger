5eccc626f38b9110f12287041b99934018bb4863
Steve Crow <steve@smcrow.net>
2020-11-27 18:11:31 -0500

    Remove unused import

--- src/main/kotlin/dev/stevecrow/vatsim/data/flight/CleanUpService.kt
+++ src/main/kotlin/dev/stevecrow/vatsim/data/flight/CleanUpService.kt
@@ -1,7 +1,6 @@
 package dev.stevecrow.vatsim.data.flight
 
 import mu.KotlinLogging
-import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
